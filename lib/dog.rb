a3e37a4ea9ec32a5be1ae46a98cf4933d56ac25f
Christina Gonzalez <cmgamway@gmail.com>
2019-09-04 19:55:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,15 +10,5 @@ end
 fido = Dog.new
 fido.name = "Fido"
 
-class Dog
-  def name=(name)
-    @name = name
-  end
-
-  def name
-    @name
-  end
-end
-fido.Dog.new
 fido.instance_variable_set(:name, "Fido")
 fido.instance_variable_get(:name)
