6fdd1c36c6fd9d647b41c391341b4d04fd9884e0
Christina Gonzalez <cmgamway@gmail.com>
2019-09-04 20:04:37 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -12,3 +12,18 @@ beyonce.name = "Beyonce"
 
 beyonce.instance_variable_set(:@name, "Beyonce")
 beyonce.instance_variable_get(:@name)
+
+class Person
+  def job=(job)
+    @job = job
+  end
+
+  def job
+    @job
+  end
+end
+beyonce = Person.new
+beyonce.job = "Singer"
+
+beyonce.instance_variable_set(:@job, "Singer")
+beyonce.instance_variable_get(:@job)
