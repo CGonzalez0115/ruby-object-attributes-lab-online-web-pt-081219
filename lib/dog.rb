275e906c3419650d38a9329a1903559cdceccf38
Christina Gonzalez <cmgamway@gmail.com>
2019-09-04 19:52:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,6 +11,13 @@ fido = Dog.new
 fido.name = "Fido"
 
 class Dog
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
 end
 fido.Dog.new("Fido")
 fido.instance_variable_set(:@name, "Fido")
