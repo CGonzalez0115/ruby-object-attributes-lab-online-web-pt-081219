e067ea2b85c6947bf2bdc0120b6c2a34586470ed
Christina Gonzalez <cmgamway@gmail.com>
2019-09-04 19:58:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,3 +12,17 @@ fido.name = "Fido"
 
 fido.instance_variable_set(:@name, "Fido")
 fido.instance_variable_get(:@name)
+
+class Dog
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+end
+snoopy = Dog.new
+
+snoopy.instance_variable_set(:@breed, "Beagle")
+snoopy.instance_variable_get(:@breed)
